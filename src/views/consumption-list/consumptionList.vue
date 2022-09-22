802aa2ac73b454d9bb462ef9100c1881895cd805
Ryan <1594741693@qq.com>
2022-09-22 18:11:48 +0800

    feat: complete my-center

--- src/views/consumption-list/consumptionList.vue
+++ src/views/consumption-list/consumptionList.vue
@@ -1,9 +1,60 @@
 <template>
     <div>
-        这里是消费列表
+        <CommSearch
+            :options-form="optionsForm"
+            @query-data="queryData"
+        />
     </div>
 </template>
 
 <script lang="ts" setup>
+    import { ref } from 'vue'
+    import CommSearch from '@/components/comm-search.vue'
+    import { FORM_TYPE } from '@/constants/common'
+
+    const optionsForm = ref([
+        {
+            type: FORM_TYPE.INPUT,
+            key: 'demo1',
+            value: '',
+            title: '输入框'
+        },
+        {
+            type: FORM_TYPE.SELECT,
+            key: 'demo2',
+            value: '',
+            title: '选择框',
+            resource: [
+                { val: '11', title: '11' },
+                { val: '22', title: '22' }
+            ]
+        },
+        {
+            type: FORM_TYPE.SELECT_FILTER,
+            key: 'demo3',
+            value: '',
+            title: '选择框过滤框',
+            resource: [
+                { val: '11', title: '我我我我我' },
+                { val: '22', title: '他他他他他' }
+            ]
+        },
+        {
+            type: FORM_TYPE.DATE_PICKER,
+            key: 'demo4',
+            value: '2022-09-22',
+            title: '日期框'
+        },
+        {
+            type: FORM_TYPE.RANGE_PICKER,
+            key: 'demo5',
+            value: ['2022-09-22', '2022-10-11'],
+            title: '日期范围框'
+        },
+    ])
+
+    const queryData = (data: any) => {
+        console.log("queryData", data)
+    }
     
 </script>
