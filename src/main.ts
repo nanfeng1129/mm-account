258ad983c7e9c08127c046176638ba108177a35b
Ryan <1594741693@qq.com>
2022-10-19 10:26:36 +0800

    fix: import message style

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/theme-chalk/el-message.css'
 import '@/assets/fonts/font.scss'
 import './styles/index.scss'
 import './styles/style.scss'
